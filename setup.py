8a7e4c4e920ee57dd15d79728ed8d6cef75bf214
Skand <pvskand@gmail.com>
2017-06-26 22:10:42 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='arXiv_download',
+      version='0.1',
+      description='View, Get Info and Download research papers from arxiv.org on your terminal!!',
+      url='https://github.com/pvskand/arXiv_download',
+      author='Skand Vishwanath Peri',
+      author_email='pvskand@gmail.com',
+      license='MIT',
+      scripts=['bin/arXiv_download'],
+      packages=['arXiv_download'],
+      install_requires=[
+          'docopt',
+      ],
+      zip_safe=False)
